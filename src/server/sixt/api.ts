6111f58ff8677381a68d8b6efeb778c678a9d4d0
Paul Köhler <koehler_paul@outlook.de>
2025-11-22 02:04:16 +0100

    Add Sixt API integration with types and helper functions

---
+++ src/server/sixt/api.ts
@@ -0,0 +1,80 @@
+import {v4 as uuidv4} from "uuid";
+import {Branch, Location, Offer, SearchRequest, SelectedLocation} from "./types";
+
+
+const baseUrl = "https://grpc-prod.orange.sixt.com";
+
+async function doGrpcRequest<T>(method: string, body: any): Promise<T> {
+    const response = await fetch(`${baseUrl}/${method}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status} ${await response.text()}`);
+    }
+
+    return await response.json() as T;
+}
+
+export async function getSuggestedLocations(query: string) {
+    const suggestedLocations = await doGrpcRequest<{
+        suggestions: {
+            location: Location,
+        }[]
+    }>(
+        "com.sixt.service.rent_booking.api.SearchService/SuggestLocations", {
+            "query": query,
+            "auto_complete_session_id": uuidv4(),
+            "vehicle_type": "1",
+            "user_profile_id": "",
+            "location_purpose": 1,
+            "include_fastlane": null
+        },
+    );
+
+    return suggestedLocations.suggestions.map(suggestion => suggestion.location);
+}
+
+export function selectLocation(branch: Branch): Promise<SelectedLocation> {
+    return doGrpcRequest<SelectedLocation>(
+        "com.sixt.service.rent_booking.api.SearchService/SelectLocation", {
+            "user_profile_id": "",
+            "location_purpose": 1,
+            "vehicle_type": 1,
+            "auto_complete_session_id": uuidv4(),
+            "location_id": branch.location_id,
+            "include_fastlane": null
+        },
+    );
+}
+
+
+export async  function getOfferRecommendations(req: SearchRequest): Promise<Offer[]> {
+    return (await doGrpcRequest<{
+        offers: Offer[]
+    }>(
+        "com.sixt.service.rent_booking.api.BookingService/GetOfferRecommendationsV2", {
+            "offer_matrix_id": uuidv4(),
+            "currency": "EUR",
+            "trip_spec": {
+                "pickup_datetime": {"value": req.pickup_timestamp.toISOString().substring(0, 16) + ":00"},
+                "pickup_location_selection_id": req.pickup_location.location_selection_id,
+                "return_location_selection_id": req.return_location.location_selection_id,
+                "point_of_sale": "DE",
+                "return_datetime": {"value": req.return_timestamp.toISOString().substring(0, 16) + ":00"},
+                "vehicle_type": 10,
+                "user_profile_id": "",
+                "corporate_customer_number": "",
+                "sim_card_country_code": "DE",
+                "device_location_country_code": "DE",
+                "campaign": ""
+            },
+            "enable_b2b_fallback": true,
+            "company_id": "1"
+        },
+    )).offers;
+}
