4f2bc4ecf50cde527ad28fdc8f57204c248e32fd
TwentyFive <twentyfivesoftware@gmail.com>
2025-11-22 15:18:50 +0100

    Add chris avatar

--- src/server/index.ts
+++ src/server/index.ts
@@ -50,7 +50,7 @@ export class SixtyAgent extends AIChatAgent<Env, AgentState> {
         availableOffers[offer.offer_id] = offer;
       }
 
-      const initialOffer = availableOffers[0]; // TEMP
+      const initialOffer = offers[0]; // TEMP
 
       this.setState({ ...this.state, initialOffer, currentOffer: initialOffer.offer_id, availableOffers });
     })();
